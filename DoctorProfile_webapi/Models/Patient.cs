0436d38d8c6684c3c43a02109c2c23276d5d79b1
Anusha <2438912@cognizant.com>
2025-10-16 15:41:33 +0530

    pat

--- DoctorProfile_webapi/Models/Patient.cs
+++ DoctorProfile_webapi/Models/Patient.cs
@@ -11,15 +11,29 @@ namespace HospitalAppointment.Models
 
         [ForeignKey("User")]
         public int UserId { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [StringLength(10)]
+        public string Phone { get; set; }
 
-       // public PatientGender Gender { get; set; } // Male, Female, Other
+        public PatientGender Gender { get; set; } // Male, Female, Other
 
         public DateTime Dob { get; set; }
 
         public string Address { get; set; }
 
-        //public User User { get; set; }
+        public User User { get; set; }
 
-       
+        public enum PatientGender
+        {
+            Male,
+            Female,
+            Other
+        }
+
+
+
     }
 }
